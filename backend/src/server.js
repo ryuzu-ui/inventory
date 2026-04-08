73ebbfb032c566ed327f370d0d9d768518422983
Aaron-Baldon <defnotjayyy06@gmail.com>
2026-04-09 02:02:49 +0800

    tan

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("/*", cors(corsOptions));
 app.use(express.json());
 
 // ✅ Startup DB check: proves which DB you are connected to
